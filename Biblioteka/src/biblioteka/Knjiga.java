8e633ed8aa6a0ec2f09c214f645f9d8265894582
Tamara Vidanovic <tamarav97@gmail.com>
2018-03-15 19:48:27 +0100

    dodati geteri i seteri

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -2,8 +2,48 @@ package biblioteka;
 
 public class Knjiga {
 
-	public Knjiga() {
-		// TODO Auto-generated constructor stub
-	}
+	
+		public String naslov;
+		public String autori;
+		public int isbn;
+		public String izdavac;
+		public int izdanje;
+		public String getNaslov() {
+			return naslov;
+		}
+		public void setNaslov(String naslov) {
+			this.naslov = naslov;
+		}
+		public String getAutori() {
+			return autori;
+		}
+		public void setAutori(String autori) {
+			this.autori = autori;
+		}
+		public int getIsbn() {
+			return isbn;
+		}
+		public void setIsbn(int isbn) {
+			this.isbn = isbn;
+		}
+		public String getIzdavac() {
+			return izdavac;
+		}
+		public void setIzdavac(String izdavac) {
+			this.izdavac = izdavac;
+		}
+		public int getIzdanje() {
+			return izdanje;
+		}
+		public void setIzdanje(int izdanje) {
+			this.izdanje = izdanje;
+		}
+		
+		
+		
+		
+		
+		
+
 
 }
